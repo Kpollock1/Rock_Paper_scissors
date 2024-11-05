3d9f2906f4a9eaf224357f99f6fcbbea7d60250c
NetherPanther <53992987+Kpollock1@users.noreply.github.com>
2024-11-05 11:42:43 +0200

    Add logic to game

--- script.js
+++ script.js
@@ -1,18 +1,58 @@
 // Get a random number and convert it into "rock, paper, or scissors"
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
-    console.log(choice)
+    
     // Pick rock, paper, scissors based on what number is generated
-    if (choice = 1) {
-        return "rock";
-    } else if (choice = 2) {
-        return "paper";
+    if (choice === 1) {
+        console.log("rock")
+        return "rock"
+    } else if (choice === 2) {
+        console.log("paper")
+        return "paper"
     } else {
+        console.log("scissors")
         return "scissors"
-    }
-}
-
-console.log(getComputerChoice())
-
+    };
     
 
+}
+
+function getHumanChoice() {
+    let choice = prompt().toLowerCase();
+
+    if (choice === "rock" 
+        || choice === "paper" 
+        || choice === "scissors") {
+        console.log(`you chose ${choice}`)
+        } else {
+            console.log("Not an option")
+        }
+    console.log(choice)
+    return choice;
+}
+
+let humanScore = 0
+let computerScore = 0
+
+
+
+function playRound(humanSelection, computerSelection) {
+
+    
+    if (humanSelection === computerSelection) {
+        console.log("It's a tie!")
+    } else if (humanScore === "rock" && computerSelection === "scissors") {
+        console.log("You win! Rock beats scissors.")
+    } else if (humanScore === "paper" && computerSelection === "rock") {
+        console.log("You win! Paper beats rock.")
+    } else if (humanScore === "scissors" && computerSelection === "paper") {
+        console.log("You win! scissors beats paper.") 
+    } else {
+        console.log("You lose.")
+    };
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound()
