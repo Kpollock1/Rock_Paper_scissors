cfb9b6e3d0e90b8cca752bdeb0c6892f4e47b86a
NetherPanther <53992987+Kpollock1@users.noreply.github.com>
2024-11-05 12:41:39 +0200

    Refactor function

--- script.js
+++ script.js
@@ -4,17 +4,12 @@ function getComputerChoice() {
     
     // Pick rock, paper, scissors based on what number is generated
     if (choice === 1) {
-        console.log("rock")
-        return "rock"
+        return "rock";
     } else if (choice === 2) {
-        console.log("paper")
-        return "paper"
+        return "paper";
     } else {
-        console.log("scissors")
-        return "scissors"
-    };
-    
-
+        return "scissors";
+    }
 }
 
 function getHumanChoice() {
@@ -23,18 +18,19 @@ function getHumanChoice() {
     if (choice === "rock" 
         || choice === "paper" 
         || choice === "scissors") {
-        console.log(`you chose ${choice}`)
+        console.log(`you chose ${choice}`);
         } else {
-            console.log("Not an option")
+            console.log("Not an option");
         }
-    console.log(choice)
+    
     return choice;
 }
 
 let humanScore = 0
 let computerScore = 0
 
-while (humanScore || computerScore < 5)
+
+
 
 function playRound() {
     const humanSelection = getHumanChoice();
@@ -44,15 +40,22 @@ function playRound() {
         console.log("It's a tie!");
     } else if (humanSelection === "rock" && computerSelection === "scissors") {
         console.log("You win! Rock beats scissors.");
+        humanScore = ++humanScore;
     } else if (humanSelection === "paper" && computerSelection === "rock") {
         console.log("You win! Paper beats rock.");
+        humanScore = ++humanScore;
     } else if (humanSelection === "scissors" && computerSelection === "paper") {
         console.log("You win! scissors beats paper.") ;
+        humanScore = ++humanScore;
     } else {
         console.log("You lose.");
-    }cs
+        computerScore = ++computerScore
+    }
 }
 
 
-
-playRound()
+while (humanScore < 5 && computerScore < 5) {
+    playRound();
+    console.log(`${humanScore} to ${computerScore}`);
+    
+}
